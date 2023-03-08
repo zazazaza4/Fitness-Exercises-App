15f0756980bb6fc29de8d3eef320e177ce58d7d7
Yaroslav <6715414@gmail.com>
2023-03-08 21:43:08 +0200

    added exports BodyPart, HorizontalScrollbar, Exercises

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
+export { HorizontalScrollbar } from "./HorizontalScrollbar";
+export { Exercises } from "./Exercises";
 export { Navbar } from "./Navbar";
 export { HeroBanner } from "./HeroBanner";
 export { SearchExercises } from "./SearchExercises";
+export { BodyPart } from "./BodyPart";
 export { Footer } from "./Footer";
