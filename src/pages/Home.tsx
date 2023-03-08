dad190769b382533f74aa290e0d379af6a75fece
Yaroslav <6715414@gmail.com>
2023-03-08 21:42:45 +0200

    added Exercises and states for Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import { Box } from "@mui/material";
 import { FC, useState } from "react";
-import { HeroBanner, SearchExercises } from "../components";
 
-export const Home: FC = () => (
-  <Box>
-    <HeroBanner />
-    <SearchExercises />
-  </Box>
-);
+import { Exercises, HeroBanner, SearchExercises } from "../components";
+import { IBodyParts, IExercise } from "../@types";
+
+export const Home: FC = () => {
+  const [bodyPart, setBodyPart] = useState<IBodyParts>(IBodyParts.ALL);
+  const [exercises, setExercises] = useState<IExercise[]>([]);
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        exercises={exercises}
+      />
+    </Box>
+  );
+};
