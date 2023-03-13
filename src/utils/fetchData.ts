3dbaccfbf15165e951d6966941b107f0e1289203
Yaroslav <6715414@gmail.com>
2023-03-13 21:48:42 +0200

    added youtubeOptions

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -7,11 +7,19 @@ interface IExerciseFetchOptions extends IFetchOptions {
   };
 }
 
-export const exerciseOptions = {
+export const exerciseOptions: IExerciseFetchOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const youtubeOptions: IExerciseFetchOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
 
