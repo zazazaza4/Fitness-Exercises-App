f973180097c9686c563d475ebc58cb84dde412ae
Yaroslav <6715414@gmail.com>
2023-03-15 23:01:28 +0200

    added Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,17 @@
-type Props = {};
+import { Box, Stack, Typography } from "@mui/material";
+import { FC } from "react";
 
-export const Footer = (props: Props) => {
-  return <div>Footer</div>;
+import Logo from "../assets/images/Logo-1.png";
+
+export const Footer: FC = () => {
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" pt="40px" mt="20px">
+          Made with &heard; by me
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
