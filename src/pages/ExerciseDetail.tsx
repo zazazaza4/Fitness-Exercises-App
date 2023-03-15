09e855911ff9dc23576c1ef55b846dd9a997b731
Yaroslav <6715414@gmail.com>
2023-03-15 23:03:43 +0200

    added ExerciseDetail page

--- src/pages/ExerciseDetail.tsx
+++ src/pages/ExerciseDetail.tsx
@@ -1,5 +1,75 @@
-type ExerciseDetailProps = {};
+import { Box } from "@mui/material";
+import { FC, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IExercise, IVideoContent, IVideoFetch } from "../@types";
+import { Detail, ExerciseVideos, SimilarExercises } from "../components";
+import {
+  BASE_URL,
+  exerciseOptions,
+  fetchData,
+  youtubeOptions,
+  YOUTUBE_SEARCH_URL,
+} from "../utils/fetchData";
 
-export function ExerciseDetail({}: ExerciseDetailProps) {
-  return <div>ExerciseDetail</div>;
-}
+interface IExerciseDetailProps {}
+
+export const ExerciseDetail: FC<IExerciseDetailProps> = () => {
+  const [exerciseDetail, setExerciseDetail] = useState<IExercise>();
+  const [exerciseVideos, setExerciseVideos] = useState<IVideoContent[]>([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState<
+    IExercise[]
+  >([]);
+  const [equipmentExercises, setEquipmentExercises] = useState<IExercise[]>([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDetailData: IExercise = await fetchData(
+        `${BASE_URL}/exercise/${id}`,
+        exerciseOptions
+      );
+
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData: IVideoFetch = await fetchData(
+        `${YOUTUBE_SEARCH_URL}/search?query=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+
+      const { contents } = exerciseVideosData;
+      setExerciseVideos(contents);
+
+      const targetMuscleExercisesData: IExercise[] = await fetchData(
+        `${BASE_URL}/exercise/target/${exerciseDetailData.target}`,
+        exerciseOptions
+      );
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equipmentExercisesData: IExercise[] = await fetchData(
+        `${BASE_URL}/exercise/equipment/${exerciseDetailData.equipment}`,
+        exerciseOptions
+      );
+      setEquipmentExercises(equipmentExercisesData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  if (!exerciseDetail) return <div>d</div>;
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
+
+      <SimilarExercises
+        targetMuscleExercises={targetMuscleExercises}
+        equipmentExercises={equipmentExercises}
+      />
+    </Box>
+  );
+};
