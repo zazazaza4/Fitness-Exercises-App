95b39c0e1015c4f7987391e614e80636490f5643
Yaroslav <6715414@gmail.com>
2023-03-15 23:02:48 +0200

    added exports for types of videos

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,3 +1,6 @@
 export type { IFetchOptions } from "./Fetch.interface";
 export { IBodyParts } from "./Fetch.interface";
 export type { IExercise } from "./Fetch.interface";
+export type { IVideoFetch } from "./Fetch.interface";
+export type { IVideo } from "./Fetch.interface";
+export type { IVideoContent } from "./Fetch.interface";
