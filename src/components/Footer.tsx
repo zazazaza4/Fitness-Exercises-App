5c1cd94a46d7f3e166f5d72760367e4519235562
Yaroslav <6715414@gmail.com>
2023-03-21 21:41:46 +0200

    fixed emoji

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,11 @@ import Logo from "../assets/images/Logo-1.png";
 
 export const Footer: FC = () => {
   return (
-    <Box mt="80px" bgcolor="#fff3f4">
+    <Box mt="80px" pb={7} bgcolor="#fff3f4">
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
         <img src={Logo} alt="logo" width="200px" height="40px" />
-        <Typography variant="h5" pt="40px" mt="20px">
-          Made with &heard; by me
+        <Typography variant="h5" pt="10px" mt="10px">
+          Made with &#128157; by me
         </Typography>
       </Stack>
     </Box>
